6d7e838c92d2c092c522b452bfb3ea950b372bc1
Sergei Antipov <mrantip90@gmail.com>
2024-03-08 21:06:19 +0300

    v7 доработан первый тест, restclient.py

--- services/mailhog.py
+++ services/mailhog.py
@@ -1,18 +1,20 @@
-import pprint
+import json
 
 import requests
+from requests import session, Response
 
 
 class MailhogApi:
-    def __init__(self, host='http://5.63.153.31:5025', headers=None):
+    def __init__(self, host='http://5.63.153.31:5025'):
         self.host = host
+        self.session = session
 
-    # self.account = AccountApi(host, headers)
-    # self.login = LoginApi(host, headers)
-
-    def get_api_v2_messages(self, limit=50):
-        url = f"{self.host}/api/v2/messages?limit=50"
-
+    def get_api_v2_messages(self, limit: int = 50) -> Response:
+        """
+        Get messages by limit
+        :param limit:
+        :return:
+        """
         response = requests.get(
             url=f"{self.host}/api/v2/messages",
             params={
@@ -22,7 +24,12 @@ class MailhogApi:
 
         return response
 
-    def get_token_from_last_email(self):
-
-response =MailhogApi().get_api_v2_messages(limit=50)
-pprint.pprint(response.json())
+    def get_token_from_last_email(self) -> str:
+        """
+        Get user activation token from last email
+        :return:
+        """
+        emails = self.get_api_v2_messages(limit=1).json()
+        token_url = json.loads(emails['items'][0]['Content']['Body'])['ConfirmationLinkUrl']
+        token = token_url.split('/')[-1]
+        return token
--- tests/test_post_v1_account.py
+++ tests/test_post_v1_account.py
@@ -1,15 +1,19 @@
 from services.dm_api_account import DmApiAccount
+from services.mailhog import MailhogApi
 
 
 def test_post_v1_account():
+    mailhog = MailhogApi(host='http://5.63.153.31:5025')
     api = DmApiAccount(host='http://5.63.153.31:5051')
     json = {
-        "login": "naruto_3",
-        "email": "naruto_3@mail.ru",
-        "password": "naruto_3_11"
+        "login": "naruto_4",
+        "email": "naruto_4@mail.ru",
+        "password": "naruto_4_11"
     }
-    response = api.account.post_v1_account(
-        json=json
-    )
+    response = api.account.post_v1_account(json=json)
+    assert response.status_code == 201, f'Статус код ответа должен быть 201, но он равен {response.status_code}'
+    token = mailhog.get_token_from_last_email()
+    response = api.account.put_v1_account_token(token=token)
+    print(token)
     print(response)
-
+    print(response.json())
